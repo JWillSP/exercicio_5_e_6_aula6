d25110e3518d62e763a7bfae786fa2de02682c72
JWillSP <46765611+JWillSP@users.noreply.github.com>
2020-05-15 17:12:09 -0300

    dasafio concluido
    desafio concluindo, falta os aperfeiçoamentos

--- exercicio_6.py
+++ exercicio_6.py
@@ -1,5 +1,16 @@
 from selenium.webdriver import Firefox
+from collections import defaultdict
 from time import sleep
+
+
+nome = ['Will', 'Paulo', 'Fábio', 'Pedro', 'Tiago']
+
+default_nome = defaultdict(lambda: iter(nome))
+
+
+def get_name(key):
+    return next(default_nome[key])
+
 
 nav = Firefox()
 
@@ -7,13 +18,21 @@ nav.get('https://selenium.dunossauro.live/exercicio_06.html')
 
 sleep(2)
 
-for i in range(5):
+while True:
 
     target_txt = nav.find_element_by_css_selector('header span').text
 
+    if target_txt == '... Mentira, você conseguiu terminar':
+
+        break
+
     t = nav.find_element_by_css_selector(f'.form-{target_txt} [type=text]')
 
-    t.send_keys('Will')
+    n = get_name(target_txt)
+
+    print(target_txt, ' ', n)
+
+    t.send_keys(n)
 
     s = nav.find_element_by_css_selector(f'.form-{target_txt} [type=password]')
 
@@ -25,4 +44,4 @@ for i in range(5):
 
     sleep(3)
 
-
+nav.close()
