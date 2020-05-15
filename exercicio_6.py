e18e3d482a5dff8dcb3e8c3be27fbd88b85cbcc4
JWillSP <j12wil@gmail.com>
2020-05-15 12:21:21 -0300

    arquivos

---
+++ exercicio_6.py
@@ -0,0 +1,28 @@
+from selenium.webdriver import Firefox
+from time import sleep
+
+nav = Firefox()
+
+nav.get('https://selenium.dunossauro.live/exercicio_06.html')
+
+sleep(2)
+
+for i in range(5):
+
+    target_txt = nav.find_element_by_css_selector('header span').text
+
+    t = nav.find_element_by_css_selector(f'.form-{target_txt} [type=text]')
+
+    t.send_keys('Will')
+
+    s = nav.find_element_by_css_selector(f'.form-{target_txt} [type=password]')
+
+    s.send_keys('45678')
+
+    e = nav.find_element_by_css_selector(f'.form-{target_txt} [type=submit]')
+
+    e.click()
+
+    sleep(3)
+
+
